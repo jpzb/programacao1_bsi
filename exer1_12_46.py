3129ca684d70f5ad5032415ba5a322337a0d8743
João Pedro Zamborlini Barcellos <jpzbarcellos@gmail.com>
2024-06-26 10:10:45 -0300

    (feature) exer1_12_46 modularizado

--- exer1_12_46.py
+++ exer1_12_46.py
@@ -1,17 +1,25 @@
+def verificaPosicao(x: float,  y:float) -> str:
+    if y < 0:
+        y *= -1
+    
+    retorno: str = ""
+
+    if (x / 3) < y < (3 * x):
+        retorno = f"x={x:.2f} y={y:.2f} INTERIOR"
+    else:
+        retorno = f"x={x:.2f} y={y:.2f} EXTERIOR"
+    return retorno
+
+
 def main() -> None:
     x: float = float(input())
     y: float = float(input())
 
     while x != 0 or y != 0:
-        if y < 0:
-            y *= -1
-        
-        if (x / 3) < y < (3 * x):
-            print(f"x={x:.2f} y={y:.2f} INTERIOR")
-        else:
-            print(f"x={x:.2f} y={y:.2f} EXTERIOR")
+        print(verificaPosicao(x, y))
         x = float(input())
         y = float(input())
     
 
-main()
+if __name__ == "__main__":
+    main()
