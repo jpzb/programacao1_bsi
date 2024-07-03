685000c9dd1d16ae358823b225c33e5987fcfb14
jpzb <jpzbarcellos@gmail.com>
2024-07-03 08:49:45 -0300

    (feature) exer1_12_44 modularizado

--- exer1_12_44.py
+++ exer1_12_44.py
@@ -1,31 +1,27 @@
-"""
-Feito por:
-Gustavo Alves Caetano
-João Pedro Zamborlini Barcellos
-"""
+def f_calcula_area_triangulo(a: float, b: float) -> float:
+    area: float = 0.0
+    if a != 0 and b != 0:
+        x_coordenada: float = (-b) / a
+        y_ordenada: float = b
+
+        if x_coordenada < 0:
+            x_coordenada *= -1
+        if y_ordenada < 0:
+            y_ordenada *= -1
+        
+        area = (x_coordenada * y_ordenada) / 2
+
+    return area
 
 
 def main() -> None:
     # Declaracao e inicializacao de variaveis
     a: float = float(input())
     b: float = float(input())
-    area: float = 0
 
     while a != 0 or b != 0:
-        if a == 0 or b == 0:
-            print(f"A: {a}, B: {b}, ÁREA: 0")
-        else:
-            x_coordenada: float = (-b) / a
-            y_ordenada: float = b
-
-            if x_coordenada < 0:
-                x_coordenada *= -1
-            if y_ordenada < 0:
-                y_ordenada *= -1
-            
-            area = (x_coordenada * y_ordenada) / 2
-
-            print(f"A: {a}, B: {b}, ÁREA: {area}")
+        area: float = f_calcula_area_triangulo(a, b)
+        print(f"AREA={area:.5f} A={a:.5f} B={b:.5f}")
     
         a = float(input())
         b = float(input())
